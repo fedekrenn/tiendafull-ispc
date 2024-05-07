a65a750e4f4f405760ab4b81226a3747503627c8
Fede Krenn <fedekrenn@gmail.com>
2024-05-07 20:13:16 -0300

    Se añaden modelos al archivo admin.py

--- backend/tiendafull/app_tiendafull/admin.py
+++ backend/tiendafull/app_tiendafull/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(TipoRol)
+admin.site.register(Usuario)
+admin.site.register(TipoModoPago)
+admin.site.register(Compra)
+admin.site.register(TipoColor)
+admin.site.register(TipoEstilo)
+admin.site.register(TipoMarca)
+admin.site.register(TipoMaterial)
+admin.site.register(TipoRodado)
+admin.site.register(Producto)
+admin.site.register(DetalleCompra)
+admin.site.register(TipoEstadoEntrega)
+admin.site.register(Entrega)
