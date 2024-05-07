d1cb70dced133ea77fb55639bae6ce17d2874110
Emiliano R Gillini <emigillini@hotmail.com>
2024-05-07 15:44:29 -0300

    GenereGaleria-Productos

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { GaleriaProductosComponent } from './components/galeria-productos/galeria-productos.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, GaleriaProductosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/components/galeria-productos/galeria-productos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria-productos',
+  standalone: true,
+  imports: [],
+  templateUrl: './galeria-productos.component.html',
+  styleUrl: './galeria-productos.component.css'
+})
+export class GaleriaProductosComponent {
+
+}
