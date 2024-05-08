40f2de0b253c741b20e01fd1b79438815f85c260
Fernando Barletta <ferbarletta@gmail.com>
2024-05-08 19:45:09 -0300

    se agrega componente login-page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { GaleriaProductosComponent } from './components/galeria-productos/galeria-productos.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, GaleriaProductosComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, GaleriaProductosComponent,LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
