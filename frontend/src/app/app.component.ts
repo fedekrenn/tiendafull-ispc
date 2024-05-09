14cda39c7da066c0b1fd1f83c1dc7a3cfd9979d4
Andres B <andresblasiche@gmail.com>
2024-05-09 01:14:55 -0300

    Se genera componente Producto

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { GaleriaProductosComponent } from './components/galeria-productos/galeria-productos.component';
+import { ProductoComponent } from './components/producto/producto.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, GaleriaProductosComponent,LoginPageComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, GaleriaProductosComponent,LoginPageComponent, ProductoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
