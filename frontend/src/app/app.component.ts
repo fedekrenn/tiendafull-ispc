9df6cc2f8a7cf620d38d972fc956050bb7308d19
Fernando Barletta <ferbarletta@gmail.com>
2024-05-09 22:34:33 -0300

    Se crea el componente customer-dashboard

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,12 +7,12 @@ import { ProductoComponent } from './components/producto/producto.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { RegistroComponent } from './components/registro/registro.component';
-
+import { CustomerDashboardComponent } from './components/customer-dashboard/customer-dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, GaleriaProductosComponent,LoginPageComponent, ProductoComponent, LandingPageComponent, RegistroComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, GaleriaProductosComponent,LoginPageComponent, ProductoComponent, LandingPageComponent, RegistroComponent,CustomerDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
