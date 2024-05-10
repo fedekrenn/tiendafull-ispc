dc206362371e6799dd77f026f4fd3a736ff65e7f
Fernando Barletta <ferbarletta@gmail.com>
2024-05-10 19:26:16 -0300

    Se actualiza componte customer-dashboard y se agregan validaciones de email y password en componente login-page

--- frontend/src/app/components/login-page/login-page.component.ts
+++ frontend/src/app/components/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {ReactiveFormsModule,FormBuilder,FormGroup} from '@angular/forms';
+import {ReactiveFormsModule,FormBuilder,FormGroup,Validators} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 
@@ -14,13 +14,21 @@ export class LoginPageComponent {
   form!:FormGroup
   contador:number = 0
   usuarioAdmin:string = 'admin@admin.com'
-  passAdmin:string = 'admin'
+  passAdmin:string = 'admin'  
   constructor(private formBuilder:FormBuilder){
     this.form = this.formBuilder.group({
-      email:'',
-      password:''
+      email:['',[Validators.required, Validators.email],[]],
+      password:['',[Validators.required],[]]
     })
   }
+  get Password() {
+    return this.form.get("password");
+  }
+  get Email() {
+    return this.form.get("email");
+  }
+
+
   validarUsuario() {
     if (this.form.value.email != "" && this.form.value.password != "") {
       this.contador = this.contador + 1;
