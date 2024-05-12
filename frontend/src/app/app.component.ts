4dc63e160242d3cafdac6d72930b18e19a92f1e0
dieguetta <diegocaminos2008@hotmail.com>
2024-05-12 14:18:09 -0300

    Se crea componente 'acerca-de-nosotros'

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,12 +7,13 @@ import { ProductoComponent } from './components/producto/producto.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { AcercaDeNosotrosComponent } from './components/acerca-de-nosotros/acerca-de-nosotros.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, GaleriaProductosComponent,LoginPageComponent, ProductoComponent, LandingPageComponent, RegistroComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, GaleriaProductosComponent,LoginPageComponent, ProductoComponent, LandingPageComponent, RegistroComponent, AcercaDeNosotrosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
