1e21153eae6563039cc5b5637cde0317529964b2
Emiliano R Gillini <emigillini@hotmail.com>
2024-05-12 17:00:38 -0300

    dashboardRoutingAnidado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,13 +7,22 @@ import { RegistroComponent } from './components/registro/registro.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { Pagina404Component } from './components/pagina-404/pagina-404.component';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { RecoverPasswordComponent } from './components/recoverpassword/recover-password.component';
+import { DetailsComponent } from './components/customer-dashboard/details/details.component';
+import { HistorialComponent } from './components/customer-dashboard/historial/historial.component';
+import { EnvioComponent } from './components/customer-dashboard/envio/envio.component';
 export const routes: Routes = [
   { path: 'Home', component: LandingPageComponent },
   { path: 'Productos', component: GaleriaProductosComponent },
   { path: 'Detalle/:id', component: ProductoComponent },
   { path: 'Login', component: LoginPageComponent },
-  { path: 'Dashboard', component: CustomerDashboardComponent },
+  { path: 'Dashboard', component: CustomerDashboardComponent, children:[
+    { path: 'Detalles', component: DetailsComponent },
+    { path: 'Historial', component: HistorialComponent },
+    { path: 'Envio', component: EnvioComponent },
+  ] },
   { path: 'Registro', component: RegistroComponent },
+  { path: 'RecoverPass', component: RecoverPasswordComponent },
   { path: 'Contact', component: ContactPageComponent },
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: '**', component: Pagina404Component },
--- frontend/src/app/components/customer-dashboard/customer-dashboard.component.ts
+++ frontend/src/app/components/customer-dashboard/customer-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-customer-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './customer-dashboard.component.html',
   styleUrl: './customer-dashboard.component.css'
 })
