91640c1cd7132b384c9d4b544f0bc96893ecc926
veronica cabrera <verosolc.30@gmail.com>
2024-05-22 01:16:52 -0300

    Comienzo a enlazar datos de los productos al componente

--- frontend/src/app/components/galeria-productos/galeria-productos.component.ts
+++ frontend/src/app/components/galeria-productos/galeria-productos.component.ts
@@ -9,4 +9,10 @@ import { RouterLink } from '@angular/router';
   templateUrl: './galeria-productos.component.html',
   styleUrl: './galeria-productos.component.css',
 })
-export class GaleriaProductosComponent {}
+export class GaleriaProductosComponent {
+  bikes = [
+    { nombre: 'Raleigh M2.0', imagen: 'raleigh-1.jpg', precio: '$200.000' },
+    {nombre: 'Trinx Gravel', imagen: 'trinx-gravel-1.jpg', precio: '$350.000',},
+    { nombre: 'Venzo Frida', imagen: 'venzo-frida-1.jpg', precio: '$189.000' },
+  ];
+}
