433be057ea94aaec67014b8337e238f8cd851345
g3rm6n <g3rm6n@yahoo.com.ar>
2024-05-25 11:20:20 -0300

    Se optimiza funcionalidad recover-password

--- frontend/src/app/components/recover-password/recover-password.component.ts
+++ frontend/src/app/components/recover-password/recover-password.component.ts
@@ -26,6 +26,14 @@ export class RecoverPasswordComponent {
   recoverPassword() {
     if (this.form.valid) {
       const email = this.form.get('email')?.value;
+      fetch("http://127.0.0.1:8000/api/recover-password/", {
+        method:"POST", 
+        headers:{"content-type":"application/json"},
+        body:
+            JSON.stringify({ // Convert the JavaScript object to a JSON string
+            email: email,
+        })
+    })
       alert('Se ha enviado un correo a ' + email);
     }
   }
