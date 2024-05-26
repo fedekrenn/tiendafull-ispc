db084ba9564e499cab28b88ce3ba887e26396427
Andres B <andresblasiche@gmail.com>
2024-05-26 05:41:40 -0300

    Data binding de product-detail

--- frontend/src/app/components/product-detail/product-detail.component.ts
+++ frontend/src/app/components/product-detail/product-detail.component.ts
@@ -1,13 +1,23 @@
-import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { ProductsService, Product } from '../../services/products.service';
 
 @Component({
   selector: 'app-producto',
   standalone: true,
-  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css',
 })
 export class ProductDetailComponent {
   @Input() id!: String;
+  bike: Product = {} as Product;
+
+  constructor(private productsService: ProductsService) {
+    this.productsService.getProduct(+this.id).subscribe((data) => {
+      this.bike = JSON.parse(JSON.stringify(data)).filter(
+        (obj: Product) => obj.id == +this.id
+      )[0];
+    });
+  }
 }
