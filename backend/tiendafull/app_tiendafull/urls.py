b16b4825c0cd0a16131cb6211a4ba77d3d1604c3
Fede Krenn <fedekrenn@gmail.com>
2024-05-29 18:57:40 -0300

    Se aplican cambios al modelo para optimizar las respuestas

--- backend/tiendafull/app_tiendafull/urls.py
+++ backend/tiendafull/app_tiendafull/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path, include
 from app_tiendafull import views
 from rest_framework import routers
-from knox import views as knox_views
+
 
 router = routers.DefaultRouter()
+
 router.register(r"products", views.ProductViewSet)
 router.register(r"cart-detail", views.CartDetailViewSet, basename="cart-detail")
-
 
 urlpatterns = [
     path("", include(router.urls)),
     path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", knox_views.LogoutView.as_view(), name="custom_logout"),
-    path("logoutall/", knox_views.LogoutAllView.as_view(), name="knox_logoutall"),
-    path("register/", views.RegistroView.as_view(), name="register"),
-    # path("cart-detail/", views.CartDetail.as_view(), name="cart-detail"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("logoutall/", views.LogoutAllView.as_view(), name="logoutall"),
+    path("register/", views.RegisterView.as_view(), name="register"),
 ]
